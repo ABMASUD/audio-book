fab2a5a1dbb25907171395f7c6d6df3ed5a3e983
ABMASUD <87662497+ABMASUD@users.noreply.github.com>
2021-08-11 17:49:33 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+
+$("h1").click(function(){
+$("h1").css("color", "#FF3F00");
+}
+);
+
+$("body").keypress(function(event){
+  $("h1").text(event.key);
+});
+
+
+$("audio").addEventlistener("keypress", function (event) {
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var letter1 = new Audio("audio/a.mp3");
+// letter1.play();
